5ef4e98ee64742da79a50cdb0113b059f3e74f6a
doanvanthanh <105765160+doanvanthanhfpt@users.noreply.github.com>
2022-09-06 06:55:45 +0700

    create_tables.py error version commit

--- create_tables.py
+++ create_tables.py
@@ -19,6 +19,15 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    #conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(DWH_ENDPOINT, DB_NAME, DB_USER, DB_PASSWORD, 5439))
+    #conn = psycopg2.connect(host=HOST,dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, port=DB_PORT)
+    #conn = psycopg2.connect("host=dwhHost dbname=dwh user=dwhuser password=Passw0rd port=5439")
+    #conn = psycopg2.connect("host=redshift-cluster-3.cknnnnpyxyzp.us-west-2.redshift.amazonaws.com dbname=dwh user=dwhuser password=Passw0rd")
+    
+    #conn="postgresql://{}:{}@{}:{}/{}".format(DB_USER, DB_PASSWORD, DWH_ENDPOINT, DB_PORT,DB_NAME)
+    #print(conn_string)
+    #%sql $conn_string
+
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
